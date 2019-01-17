87a75c72e68f6ce1149ab969fa6ad58154846b6c
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2019-01-17 22:31:51 +0900

    Initial Commit

---
+++ SpeechDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeechDemo
+//
+//  Created by kensuke-hoshikawa on 2019/01/17.
+//  Copyright © 2019 star__hoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
